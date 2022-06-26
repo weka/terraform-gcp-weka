e23e8a519fb038c85ccd95a8c0243d84089aac95
Assaf Giladi <assaf@weka.io>
2022-06-26 10:15:49 +0300

    fix: parse instance creation time properly in terminate cloud function

--- cloud-functions/terminate/terminate_test.go
+++ cloud-functions/terminate/terminate_test.go
@@ -3,8 +3,17 @@ package terminate
 import (
 	"fmt"
 	"testing"
+	"time"
 )
 
 func Test_Terminate(t *testing.T) {
+	_, err := time.Parse(time.RFC3339, "2022-06-21T21:59:55.156-07:00")
+
+	if err != nil {
+		t.Logf("error formatting creation time %s", err.Error())
+	} else {
+		t.Log("Formatting succeeded")
+	}
+
 	fmt.Println("ToDo: write test")
 }
