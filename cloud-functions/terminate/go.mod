24a65eaaa57088ba394e32f40bd7a6a8914547fd
Assaf Giladi <assaf@weka.io>
2022-06-27 17:09:40 +0300

    fix: add missing counter decrement on instance termination

--- cloud-functions/terminate/go.mod
+++ cloud-functions/terminate/go.mod
@@ -4,6 +4,8 @@ go 1.16
 
 require (
 	cloud.google.com/go/compute v1.7.0
+	cloud.google.com/go/firestore v1.6.1
+	firebase.google.com/go v3.13.0+incompatible
 	github.com/google/uuid v1.3.0
 	github.com/rs/zerolog v1.27.0
 	golang.org/x/oauth2 v0.0.0-20220608161450-d0670ef3b1eb
--- cloud-functions/terminate/terminate.go
+++ cloud-functions/terminate/terminate.go
@@ -2,9 +2,11 @@ package terminate
 
 import (
 	compute "cloud.google.com/go/compute/apiv1"
+	"cloud.google.com/go/firestore"
 	"context"
 	"encoding/json"
 	"errors"
+	firebase "firebase.google.com/go"
 	"fmt"
 	"github.com/rs/zerolog/log"
 	"github.com/weka/gcp-tf/cloud-functions/terminate/protocol"
@@ -294,7 +296,40 @@ func writeResponse(w http.ResponseWriter, response protocol.TerminatedInstancesR
 	json.NewEncoder(w).Encode(response)
 }
 
+func deccrement(project, collectionName, documentName string, amount int) (err error) {
+	log.Info().Msg("updating DB")
+
+	ctx := context.Background()
+	conf := &firebase.Config{ProjectID: project}
+	app, err := firebase.NewApp(ctx, conf)
+	if err != nil {
+		log.Error().Msgf("%s", err)
+		return
+	}
+
+	client, err := app.Firestore(ctx)
+	if err != nil {
+		log.Error().Msgf("%s", err)
+		return
+	}
+	defer client.Close()
+	doc := client.Collection(collectionName).Doc(documentName)
+
+	_, err = doc.Update(ctx, []firestore.Update{
+		{Path: "counter", Value: firestore.Increment(amount * -1)},
+	})
+
+	if err != nil {
+		log.Error().Msgf("Failed updating db: %s", err)
+	}
+
+	return
+}
+
 func Terminate(w http.ResponseWriter, r *http.Request) {
+	project := os.Getenv("PROJECT")
+	collectionName := os.Getenv("COLLECTION_NAME")
+	documentName := os.Getenv("DOCUMENT_NAME")
 
 	var response protocol.TerminatedInstancesResponse
 	var err error
@@ -355,6 +390,8 @@ func Terminate(w http.ResponseWriter, r *http.Request) {
 
 	terminatedInstances, errs := terminateUnneededInstances(instanceGroup, candidatesToTerminate, scaleResponse.ToTerminate)
 	response.AddTransientErrors(errs)
+
+	deccrement(project, collectionName, documentName, len(terminatedInstances))
 
 	//detachTerminated(asgName)
 
