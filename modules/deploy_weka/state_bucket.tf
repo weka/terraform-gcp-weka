836f70b53bfd3d8a5f6e428bb6761515fa082306
Assaf Giladi <assaf@weka.io>
2022-07-05 14:00:35 +0300

    fix: add ignore changes to state storage object

--- modules/deploy_weka/state_bucket.tf
+++ modules/deploy_weka/state_bucket.tf
@@ -7,4 +7,8 @@ resource "google_storage_bucket_object" "state" {
   name   = "state"
   bucket = google_storage_bucket.state_bucket.name
   content = "{\"initial_size\":${var.cluster_size}, \"desired_size\":${var.cluster_size}, \"instances\":[]}"
+
+  lifecycle {
+    ignore_changes = [content]
+  }
 }
