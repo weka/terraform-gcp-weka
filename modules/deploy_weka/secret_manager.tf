8287b7e3f72a15e2641622a733aade933c1fcb74
Assaf Giladi <assaf@weka.io>
2022-07-20 18:17:04 +0300

    fix: don't create token secret on private network

--- modules/deploy_weka/secret_manager.tf
+++ modules/deploy_weka/secret_manager.tf
@@ -38,6 +38,7 @@ resource "google_secret_manager_secret_version" "user_secret_key" {
 }
 
 resource "google_secret_manager_secret" "secret_token" {
+  count = var.private_network ? 0 : 1
   secret_id = "${var.prefix}-${var.cluster_name}-token"
   replication {
     automatic = true
@@ -46,7 +47,8 @@ resource "google_secret_manager_secret" "secret_token" {
 }
 
 resource "google_secret_manager_secret_version" "token_secret_key" {
-  secret      = google_secret_manager_secret.secret_token.id
+  count = var.private_network ? 0 : 1
+  secret      = google_secret_manager_secret.secret_token[count.index].id
   secret_data = var.get_weka_io_token
 
   lifecycle {
