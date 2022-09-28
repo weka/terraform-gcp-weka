4c3ed542edaad6a31751b7a573d4fd7c98ff055e
Assaf Giladi <assaf@weka.io>
2022-09-28 20:28:42 +0300

    fix: add missing compute api enablement

--- modules/deploy_weka/gcp_services_api.tf
+++ modules/deploy_weka/gcp_services_api.tf
@@ -25,3 +25,11 @@ resource "google_project_service" "service-scheduler-api" {
   disable_on_destroy = false
   disable_dependent_services = false
 }
+
+# for google_compute_region_health_check
+resource "google_project_service" "compute-api" {
+  project = var.project
+  service = "compute.googleapis.com"
+  disable_on_destroy = false
+  disable_dependent_services = false
+}
