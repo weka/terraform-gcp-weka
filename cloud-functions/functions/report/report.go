fe71ec9efab9bd8d3d7abd52869dcff25fdbc6da
Assaf Giladi <assaf@weka.io>
2023-07-25 13:42:24 +0300

    fix: lock state when reporting

--- cloud-functions/functions/report/report.go
+++ cloud-functions/functions/report/report.go
@@ -1,23 +1,44 @@
 package report
 
 import (
+	"cloud.google.com/go/storage"
 	"context"
 	"github.com/rs/zerolog/log"
 	"github.com/weka/gcp-tf/modules/deploy_weka/cloud-functions/common"
 	"github.com/weka/go-cloud-lib/protocol"
 	reportLib "github.com/weka/go-cloud-lib/report"
+	"time"
 )
 
 func Report(ctx context.Context, report protocol.Report, bucket string) (err error) {
 	log.Info().Msgf("Updating state %s with %s", report.Type, report.Message)
-	state, err := common.GetClusterState(ctx, bucket)
+	client, err := storage.NewClient(ctx)
+	if err != nil {
+		log.Error().Msgf("Failed creating storage client: %s", err)
+		return
+	}
+	defer client.Close()
+
+	id, err := common.Lock(client, ctx, bucket)
+	for err != nil {
+		time.Sleep(1 * time.Second)
+		id, err = common.Lock(client, ctx, bucket)
+	}
+
+	stateHandler := client.Bucket(bucket).Object("state")
+
+	state, err := common.ReadState(stateHandler, ctx)
 	if err != nil {
 		return
 	}
+
 	err = reportLib.UpdateReport(report, &state)
 	if err != nil {
 		return
 	}
-	err = common.UpdateClusterState(ctx, bucket, state)
+
+	err = common.WriteState(stateHandler, ctx, state)
+
+	err = common.Unlock(client, ctx, bucket, id)
 	return
 }
