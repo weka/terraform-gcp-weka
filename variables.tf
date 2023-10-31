7f17946673c887fc1d511fda34c96fbd6aa5f427
Assaf Giladi <assaf@weka.io>
2023-10-31 09:12:51 +0200

    chore: update default weka version 4.2.5

--- variables.tf
+++ variables.tf
@@ -86,7 +86,7 @@ variable "install_url" {
 variable "weka_version" {
   type        = string
   description = "Weka version"
-  default = "4.2.4"
+  default = "4.2.5"
 }
 
 variable "weka_username" {
