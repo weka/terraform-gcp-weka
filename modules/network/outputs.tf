c5aaf19ff9e566c5f0fcd0d1f1d6472593a32396
Denise Perez <densie@weka.io>
2024-01-15 17:18:21 +0200

    feat: Support skipping subnets and subnets related resources of network module

--- modules/network/outputs.tf
+++ modules/network/outputs.tf
@@ -4,22 +4,22 @@ output "vpcs_names" {
 }
 
 output "gateway_address" {
-  value       = length(var.subnets) == 0 ? [for g in google_compute_subnetwork.subnetwork : g.gateway_address] : [for g in data.google_compute_subnetwork.subnets_list_ids : g.gateway_address]
+  value       = length(var.subnets) == 0 ? length(var.subnets_range) > 0 ? [for g in google_compute_subnetwork.subnetwork : g.gateway_address] : [] : [for g in data.google_compute_subnetwork.subnets_list_ids : g.gateway_address]
   description = "List of vpcs gateway addresses"
 }
 
 output "subnetwork_name" {
-  value       = length(var.subnets) == 0 ? [for s in google_compute_subnetwork.subnetwork : s.name] : [for s in data.google_compute_subnetwork.subnets_list_ids : s.name]
+  value       = length(var.subnets) == 0 ? length(var.subnets_range) > 0 ? [for s in google_compute_subnetwork.subnetwork : s.name] : [] : [for s in data.google_compute_subnetwork.subnets_list_ids : s.name]
   description = "List of vpcs subnets names"
 }
 
 output "subnets_range" {
-  value       = length(var.subnets) == 0 ? var.subnets_range : [for i in data.google_compute_subnetwork.subnets_list_ids : i.ip_cidr_range]
+  value       = length(var.subnets) == 0 ? length(var.subnets_range) > 0 ? var.subnets_range : [] : [for i in data.google_compute_subnetwork.subnets_list_ids : i.ip_cidr_range]
   description = "List of vpcs subnets ranges"
 }
 
 output "vpc_connector_id" {
-  value       = var.vpc_connector_id == "" ? google_vpc_access_connector.connector[0].id : ""
+  value       = var.vpc_connector_id == "" && var.vpc_connector_range != "" ? google_vpc_access_connector.connector[0].id : ""
   description = "Vpc connector id"
 }
 
