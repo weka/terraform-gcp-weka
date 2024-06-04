be2efbd58575882d12ad7bf24e3343bc142552e1
Kristina Solovyova <kristina.solovyova@weka.io>
2024-06-04 11:07:08 +0300

    fix: make fetch func use protocol structs from go-cloud-lib

--- cloud-functions/functions/fetch/fetch.go
+++ cloud-functions/functions/fetch/fetch.go
@@ -6,26 +6,12 @@ import (
 
 	"cloud.google.com/go/compute/apiv1/computepb"
 	"github.com/weka/gcp-tf/modules/deploy_weka/cloud-functions/common"
+	"github.com/weka/go-cloud-lib/protocol"
 )
-
-type HgInstance struct {
-	Id        string
-	PrivateIp string
-}
-
-type HostGroupInfoResponse struct {
-	Username        string       `json:"username"`
-	Password        string       `json:"password"`
-	DesiredCapacity int          `json:"desired_capacity"`
-	Instances       []HgInstance `json:"instances"`
-	BackendIps      []string     `json:"backend_ips"`
-	Role            string       `json:"role"`
-	Version         int          `json:"version"`
-}
 
 func GetFetchDataParams(
 	ctx context.Context, project, zone, instanceGroup, bucket, usernameId, passwordId string,
-) (hostGroupInfoResponse HostGroupInfoResponse, err error) {
+) (hostGroupInfoResponse protocol.HostGroupInfoResponse, err error) {
 	creds, err := common.GetUsernameAndPassword(ctx, usernameId, passwordId)
 	if err != nil {
 		return
@@ -41,23 +27,22 @@ func GetFetchDataParams(
 		return
 	}
 
-	hostGroupInfoResponse = HostGroupInfoResponse{
-		Username:        creds.Username,
-		Password:        creds.Password,
-		DesiredCapacity: desiredCapacity,
-		Instances:       getHostGroupInfoInstances(instances),
-		BackendIps:      common.GetInstanceGroupBackendsIps(instances),
-		Role:            "backend",
-		Version:         1,
+	hostGroupInfoResponse = protocol.HostGroupInfoResponse{
+		Username:                    creds.Username,
+		Password:                    creds.Password,
+		WekaBackendsDesiredCapacity: desiredCapacity,
+		WekaBackendInstances:        getHostGroupInfoInstances(instances),
+		BackendIps:                  common.GetInstanceGroupBackendsIps(instances),
+		Role:                        "backend",
+		Version:                     1,
 	}
-
 	return
 }
 
-func getHostGroupInfoInstances(instances []*computepb.Instance) (ret []HgInstance) {
+func getHostGroupInfoInstances(instances []*computepb.Instance) (ret []protocol.HgInstance) {
 	for _, i := range instances {
 		if i.Id != nil && len(i.NetworkInterfaces) > 0 {
-			ret = append(ret, HgInstance{
+			ret = append(ret, protocol.HgInstance{
 				Id:        fmt.Sprintf("%d", i.Id),
 				PrivateIp: *i.NetworkInterfaces[0].NetworkIP,
 			})
