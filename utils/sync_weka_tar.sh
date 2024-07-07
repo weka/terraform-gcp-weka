c46876eb18c8edf3ed4471ff1a56553f837347fe
Denise Perez <densie@weka.io>
2024-07-07 16:16:13 +0300

    feat: support rocky 8 repo

--- utils/sync_weka_tar.sh
+++ utils/sync_weka_tar.sh
@@ -2,7 +2,7 @@
 
 # you must pass get.weka.io token i.e. "TOKEN" as env var
 
-version="4.1.0"
+version="4.2.11"
 bucket_name="weka-installation"
 
 curl -LO "https://$TOKEN@get.weka.io/dist/v1/pkg/weka-$version.tar"
