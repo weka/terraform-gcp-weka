5143bb5d0fba49885981c206f9fc5f07dd3ddeed
Assaf Giladi <assaf@weka.io>
2024-07-22 09:47:45 +0300

    feat: support other base branch for new release branch

--- release/create_new_release_branch.sh
+++ release/create_new_release_branch.sh
@@ -7,15 +7,22 @@ set -ex
 # prerequisites: github cli: https://cli.github.com/
 
 export new_weka_version="$1"
+export base_branch="$2"
+
 if [ -z "$new_weka_version" ]
 then
       echo "Please provide the new weka version"
       exit 1
 fi
 
-git checkout dev
+if [ -z "$base_branch" ]
+then
+      base_branch="dev"
+fi
+
+git checkout "$base_branch"
 git pull
-git checkout -b "dev-$new_weka_version"
+git checkout -b "$base_branch-$new_weka_version"
 old_weka_version=$(awk '/WEKA version/{getline;print $NF;}' variables.tf | tr -d \")
 
 file_paths=(
@@ -33,6 +40,7 @@ for file_path in "${file_paths[@]}"; do
 done
 
 git commit -m "chore: update weka default version: $new_weka_version"
-git push --set-upstream origin "dev-$new_weka_version"
-gh pr create --base main --title "Dev $new_weka_version" --body ""
+git push --set-upstream origin "$base_branch-$new_weka_version"
+capitalized_base_branch=$(echo "$base_branch" | awk '{print toupper(substr($0, 1, 1)) tolower(substr($0, 2))}')
+gh pr create --base main --title "$capitalized_base_branch $new_weka_version" --body ""
 gh pr view --web
