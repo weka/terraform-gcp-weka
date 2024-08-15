3c149fc43678b0a8c15a8a011e85206257a374cf
Joe <joe@fluidnumerics.com>
2024-08-15 13:26:45 -0400

    Upgrade google provider to 5.11.x
    This is needed for compatibility in the schedmd-slurm-gcp-v6-nodeset
    module in Google Cloud's cluster toolkit ( See
    https://github.com/GoogleCloudPlatform/cluster-toolkit/blob/main/community/modules/compute/schedmd-slurm-gcp-v6-nodeset/versions.tf
    )

--- modules/clients/versions.tf
+++ modules/clients/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~>4.84.0"
+      version = "~>5.11.0"
     }
   }
   required_version = ">=1.3.1"
