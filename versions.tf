66255a877b7da2501f56b793eaef39304b12f617
Joe <joe@fluidnumerics.com>
2024-08-15 13:31:08 -0400

    Update google provider to 5.11.x in main module interface

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "~>4.84.0"
+      version = "~>5.11.0"
     }
     archive = {
       source  = "hashicorp/archive"
