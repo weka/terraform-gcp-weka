2a0e0ad49a19d0558e70aed07a991235b1bf950f
Assaf Giladi <assaf@weka.io>
2024-09-05 19:45:27 +0300

    chore: update release scripts

--- release/create_draft_release.sh
+++ release/create_draft_release.sh
@@ -2,31 +2,23 @@
 set -ex
 
 # This script will be called when a new content was merged to the main branch.
-# It will create a new draft release with the new weka version.
+# It will create a new draft release.
+export new_tag="$1"
 
-export new_weka_version="$1"
+if [ -z "$new_tag" ]
+then
+      echo "Calculating next tag"
+      source release/calculate_next_tag.sh
+else
+      echo "Using provided tag: $new_tag"
+fi
 
 git checkout main
 git pull
-git fetch --tags
-latest_tag=$(git describe --tags "$(git rev-list --tags --max-count=1)")
-echo "latest tag: $latest_tag"
-major=$(echo "$latest_tag" | cut -d. -f1)
-major="${major:1}" # remove v
-minor=$(echo "$latest_tag" | cut -d. -f2)
-patch=$(echo "$latest_tag" | cut -d. -f3)
-new_patch=$((patch + 1))
 
-new_tag="v$major.$minor.$new_patch"
-echo "new tag: $new_tag"
 git tag "$new_tag"
 git push origin "$new_tag"
 
-if [ -z "$new_weka_version" ]
-then
-      message="Fill release notes here."
-else
-      message="Updated WEKA default version to $new_weka_version"
-fi
+message="Fill release notes here."
 
 gh release create "$new_tag" --draft --title "$new_tag" --notes "- $message"
