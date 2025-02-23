82ec5e09abc8520b3b50b3dd35211adb8686e622
Assaf Giladi <assaf@weka.io>
2025-02-23 14:33:10 +0200

    chore: update google providers

--- modules/network/versions.tf
+++ modules/network/versions.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">=6.12.0"
+      version = ">=6.21.0"
     }
     google-beta = {
       source  = "hashicorp/google-beta"
-      version = "~> 3.83.0"
+      version = ">=6.21.0"
     }
   }
   required_version = ">=1.3.1"
